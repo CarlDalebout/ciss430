9be64f8e95ba557a7e3b3842aa795e2ee0c935e2
Carl <cadalebout1@ccis.edu>
2025-01-16 09:59:47 -0600

    almost finished with a01

--- a/a01/bufferpool.h
+++ a/a01/bufferpool.h
@@ -5,6 +5,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cerrno>
+#include <cstring>
 
 
 struct FRAME
@@ -14,7 +20,7 @@ struct FRAME
   std::string page = "";
 };
 
-class BufferPool
+class BufferPool // using fstream
 {
   public:
     BufferPool(std::string file, int framesize = 4, int frames = 3)
@@ -25,13 +31,13 @@ class BufferPool
     //setters
     std::string& operator [](int index) { return frameBuffer_[index].page; } // setter
     void setFrameSize(int size) { framesize_ = size; }
-    // int setFrameSize(int n) { framesize_ = size; }
+    void resize(int n);
     void writePage(int pagenumber, std::string frame);
 
     //getters
     const std::string& operator [](int index) const { return frameBuffer_[index].page; } // getter
-    int getFrameSize() {  return framesize_; }
-    int getFrames()    {  return frames_; }
+    int getFrameSize()                              {  return framesize_; }
+    int size()                                      {  return frames_; }
     std::string getPage(int pagenumber);
 
     void run();
@@ -46,6 +52,52 @@ class BufferPool
   std::string newFrame_;
   
   std::vector<FRAME> frameBuffer_;
+};
+
+class BufferPool2
+{
+  public:
+    BufferPool2(const char* filename, int framesize = 4, int frames = 3)
+    :file_(open(filename, O_RDWR)), framesize_(framesize), frames_(frames),
+     frameBuffer_(std::vector<FRAME>(frames_))
+    {
+      if (file_ < 0)
+      {
+      std::cout << "open fail\n";
+      std::cout << "errno: " << errno << std::endl;
+      std::cout << "strerror: " << strerror(errno) << std::endl;
+      }
+      else
+      {
+      std::cout << "open ok" << std::endl;
+      }
+    }
+    ~BufferPool2()
+    {
+      std::cout << "closed file_\n";
+      close(file_);
+    }
+
+    //setters
+    std::string& operator [](int index) { return frameBuffer_[index].page; } // setter
+    void setFrameSize(int size) { framesize_ = size; }
+    void resize(int n);
+    void writePage(int file, int pagenumber, std::string frame);
+
+    //getters
+    const std::string& operator [](int index) const { return frameBuffer_[index].page; } // getter
+    int getFrameSize() {  return framesize_; }
+    int size()         {  return frames_; }
+    std::string getPage(int file, int pagenumber);
+
+    void run();
+
+  private:
+    
+    int file_;
+    int framesize_;
+    int frames_;
+    std::vector<FRAME> frameBuffer_;
 };
 
 #endif
--- a/a01/main.cpp
+++ a/a01/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <string>
 #include "bufferpool.h"
 
 
  
 int main()
 {
-  BufferPool buffer("abc.txt");
+  std::string file = "abc.txt";
+  const char* filename = file.c_str();
+  BufferPool2 buffer(filename);
+  
   buffer.run();
-
+  
   return 0;
 }
